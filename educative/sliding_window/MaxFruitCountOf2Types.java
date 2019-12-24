dda2badeced73ff71c4b8c693803d7ef9ea48f0e
Khalid Ali <khalludi123@gmail.com>
2019-12-23 19:00:43 -0500

    Add main method to fruit basket problem

--- educative/sliding_window/MaxFruitCountOf2Types.java
+++ educative/sliding_window/MaxFruitCountOf2Types.java
@@ -24,5 +24,12 @@ class MaxFruitCountOf2Types {
     
     return ret;
   }
+
+  public static void main(String[] args) {
+    System.out.println("Maximum number of fruits: " +
+                          MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+    System.out.println("Maximum number of fruits: " +
+                          MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+  }
 }
 
