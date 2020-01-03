0752d6ca570954dc46800ad0e12665650ea9fe77
Khalid Ali <khalludi123@gmail.com>
2020-01-03 11:24:49 -0500

    Count triplets with smaller sum than target

---
+++ educative/two_pointers/TripletWithSmallerSum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class TripletWithSmallerSum {
+
+  public static int searchTriplets(int[] arr, int target) {
+    int count = 0;
+    Arrays.sort(arr);
+
+    for(int i = 0; i < arr.length - 2; i++) {
+      int left = i + 1, right = arr.length - 1;
+      while (left < right) {
+        int sum = arr[i] + arr[left] + arr[right];
+        if (sum < target) {
+          count += right-left;
+          left++;
+        } else if (sum >= target) {
+          right--;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(TripletWithSmallerSum.searchTriplets(new int[] { -1, 0, 2, 3 }, 3));
+    System.out.println(TripletWithSmallerSum.searchTriplets(new int[] { -1, 4, 2, 1, 3 }, 5));
+  }
+}
